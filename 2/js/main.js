cd6beabddead1d28031e899c65962dc9db9575e9
Keks <keks@htmlacademy.ru>
2020-06-07 20:38:29 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,11 +1,21 @@
 'use strict';
 
+var ADS_QUANTITY = 8;
+var AD_WIDTH = 50;
+var AD_HEIGHT = 70;
+
 var titles = ['первый', 'второй', 'третий', 'четвертый', 'пятый', 'шестой', 'седьмой', 'восьмой'];
 var type = ['palace', 'flat', 'house', 'bungalo'];
 var checkin = ['12:00', '13:00', '14:00'];
+var checkout = checkin;
 var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var description = ['новая', 'модель', 'организационной', 'деятельности', 'позволяет', 'выполнять'];
-// var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
+var map = document.querySelector('.map');
+var mapPins = document.querySelector('.map__pins');
+
+map.classList.remove('map--faded');
 
 // случайное значениe массива
 var getRandomArrayIndex = function (array) {
@@ -18,59 +28,39 @@ var getRandomNumber = function (min, max) {
   return Math.round(random);
 };
 
-// случайная длина массива
-// var getRandomArrayLength = function (array) {
-
-// на эту строку ругается npm test: 'Return statement should not contain assignment '
-// return array.length = [Math.round(Math.random() * array.length)];
-// };
-
-// перемешивание массива
-// var shuffleArray = function (array) {
-//   for (var i = array.length - 1; i > 0; i--) {
-//     var j = Math.floor(Math.random() * (i + 1));
-
-//     // на эту строку ругается npm test: "Parsing error: Assigning to rvalue"
-//     // [array[i], array[j]] = [array[j], array[i]];
-//   }
-//   return array;
-// };
-
 var ads = [];
 
-var featuresLength = 6;
-features.length = getRandomNumber(1, featuresLength);
-
+// случайная длина массива
+var getRandomArray = function (array) {
+  return array.slice(Math.round(Math.random() * array.length));
+};
 
 var createArray = function (array) {
-  for (var i = 0; i < 8; i++) {
+
+  for (var i = 1; i <= ADS_QUANTITY; i++) {
+    var addressX = getRandomNumber(100, 1200);
+    var addressY = getRandomNumber(130, 630);
+
     array.push({
       'author': {
-        'avatar': 'img/avatars/user' + '0' + (i + 1) + '.png'
+        'avatar': 'img/avatars/user0' + i + '.png'
       },
       'offer': {
-        'title': titles[i],
-
-        // не работает
-        // 'address': 2,
-
+        'title': getRandomNumber(1, titles.length),
+        'address': addressX + ', ' + addressY,
         'price': getRandomNumber(100, 10000),
         'type': getRandomArrayIndex(type),
         'rooms': getRandomNumber(1, 5),
         'guests': getRandomNumber(1, 10),
         'checkin': getRandomArrayIndex(checkin),
-
-        // не работает
-        'features': features,
-
+        'checkout': getRandomArrayIndex(checkout),
+        'features': getRandomArray(features),
         'description': getRandomArrayIndex(description),
-
-        // не работает
-        // 'photos': getRandomArrayLength(photos)[i + Math.round(Math.random() * photos.length)]
+        'photos': getRandomArray(photos)
       },
       'location': {
-        'x': getRandomNumber(100, 1200),
-        'y': getRandomNumber(130, 630)
+        'x': addressX,
+        'y': addressY
       }
     });
   }
@@ -79,27 +69,31 @@ var createArray = function (array) {
 };
 
 createArray(ads);
-// console.log(createArray(ads));
 
 var adTemplate = document.querySelector('#pin')
   .content
   .querySelector('.map__pin');
 
-
-// не работает
-var renderAd = function (ad) {
+var createAd = function (ad) {
   var adItem = adTemplate.cloneNode(true);
-  adItem.querySelector('.map__pin').src = ad.avatar;
-  adItem.querySelector('.map__pin').style = 'left: 200px top: 300px;';
-  adItem.querySelector('.map__pin').alt = ad.title;
+  var adItemImage = adTemplate.querySelector('img');
+
+  adItem.style.left = ad.location.x - AD_WIDTH / 2 + 'px';
+  adItem.style.top = ad.location.y - AD_HEIGHT + 'px';
+
+  adItemImage.src = ad.author.avatar;
+  adItemImage.alt = ad.offer.title;
+
   return adItem;
 };
 
-var fragment = document.createDocumentFragment();
+/* Отрисовывает метки*/
+var renderAds = function () {
+  var fragment = document.createDocumentFragment();
+  for (var j = 0; j < ads.length; j++) {
+    fragment.appendChild(createAd(ads[j]));
+  }
+  mapPins.appendChild(fragment);
+};
 
-// не работает
-for (var j = 0; j < ads.length; j++) {
-  fragment.appendChild(renderAd(ads[j]));
-}
-
-adTemplate.appendChild(fragment);
+renderAds();
