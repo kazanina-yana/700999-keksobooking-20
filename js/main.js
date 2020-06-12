9cff81da54ff863c359f7b48cebd61cee9ff1719
makemanback <makemanback@gmail.com>
2020-06-12 13:59:20 +0300

    Личный проект: больше деталей (часть 2)

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.g
 
 var map = document.querySelector('.map');
 var mapPins = document.querySelector('.map__pins');
+var mapFilters = document.querySelector('.map__filters-container');
 
 map.classList.remove('map--faded');
 
@@ -46,7 +47,7 @@ var createArray = function (array) {
         'avatar': 'img/avatars/user0' + i + '.png'
       },
       'offer': {
-        'title': getRandomNumber(1, titles.length),
+        'title': getRandomArrayIndex(titles),
         'address': addressX + ', ' + addressY,
         'price': getRandomNumber(100, 10000),
         'type': getRandomArrayIndex(type),
@@ -76,7 +77,7 @@ var adTemplate = document.querySelector('#pin')
 
 var createAd = function (ad) {
   var adItem = adTemplate.cloneNode(true);
-  var adItemImage = adTemplate.querySelector('img');
+  var adItemImage = adItem.querySelector('img');
 
   adItem.style.left = ad.location.x - AD_WIDTH / 2 + 'px';
   adItem.style.top = ad.location.y - AD_HEIGHT + 'px';
@@ -87,7 +88,6 @@ var createAd = function (ad) {
   return adItem;
 };
 
-/* Отрисовывает метки*/
 var renderAds = function () {
   var fragment = document.createDocumentFragment();
   for (var j = 0; j < ads.length; j++) {
@@ -97,3 +97,95 @@ var renderAds = function () {
 };
 
 renderAds();
+
+// функция фото
+var getPhotos = function (array, block) {
+  var fragmentPhoto = document.createDocumentFragment();
+
+  block.innerHTML = '';
+
+  for (var i = 0; i < array.length; i++) {
+    var photo = document.createElement('img');
+    photo.src = array[i];
+    photo.width = 45;
+    photo.classList.add('popup__photo');
+
+    fragmentPhoto.appendChild(photo);
+  }
+
+  return block.appendChild(fragmentPhoto);
+};
+
+// карточка объявления
+var cardTemplate = document.querySelector('#card')
+  .content
+  .querySelector('.map__card');
+
+var createMapCard = function (ad) {
+  var mapCard = cardTemplate.cloneNode(true);
+  var mapCardTitle = mapCard.querySelector('.popup__title');
+  var mapCardAdress = mapCard.querySelector('.popup__text--address');
+  var mapCardPrice = mapCard.querySelector('.popup__text--price');
+  var mapCardType = mapCard.querySelector('.popup__type');
+  var mapCardCapacity = mapCard.querySelector('.popup__text--capacity');
+  var mapCardTime = mapCard.querySelector('.popup__text--time');
+  var mapCardDescription = mapCard.querySelector('.popup__description');
+  var mapCardAvatar = mapCard.querySelector('.popup__avatar');
+  var mapCardFeaturesContainer = mapCard.querySelector('.popup__features');
+  var mapCardFeatures = mapCardFeaturesContainer.children;
+  var mapCardPhotosContainer = mapCard.querySelector('.popup__photos');
+
+  // иконки фич
+  for (var i = 0; i < mapCardFeatures.length; i++) {
+    var remove = true;
+    for (var j = 0; j < ad.offer.features.length; j++) {
+      var featureClass = 'popup__feature--' + ad.offer.features[j];
+      if (mapCardFeatures[i].classList.contains(featureClass)) {
+        remove = false;
+      }
+    }
+    if (remove) {
+      mapCardFeaturesContainer.removeChild(mapCardFeatures[i]);
+      i = i - 1;
+    }
+  }
+
+  mapCardTitle.textContent = ad.offer.title;
+  mapCardAdress.textContent = ad.offer.address;
+  mapCardPrice.textContent = ad.offer.price + '₽/ночь';
+  mapCardCapacity.textContent = ad.offer.rooms + ' комнаты для ' + ad.offer.guests + ' гостей';
+  mapCardTime.textContent = 'Заезд после ' + ad.offer.checkin + ', выезд до ' + ad.offer.checkout;
+  mapCardDescription.textContent = ad.offer.description;
+  mapCardAvatar.src = ad.author.avatar;
+
+
+  // фотографии
+  getPhotos(ad.offer.photos, mapCardPhotosContainer);
+
+  // типы жилья
+  switch (ad.offer.type) {
+    case ('flat'):
+      mapCardType.textContent = 'квартира';
+      break;
+    case ('bungalo'):
+      mapCardType.textContent = 'бунгало';
+      break;
+    case ('house'):
+      mapCardType.textContent = 'дом';
+      break;
+    case ('palace'):
+      mapCardType.textContent = 'дворец';
+      break;
+  }
+
+
+  return mapCard;
+};
+
+var renderMapCard = function () {
+  var fragmentSecond = document.createDocumentFragment();
+  fragmentSecond.appendChild(createMapCard(ads[0]));
+  map.insertBefore(fragmentSecond, mapFilters);
+};
+
+renderMapCard();
