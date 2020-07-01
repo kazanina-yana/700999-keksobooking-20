dbb481a045ca21f709d49290935e5d50a4a997ed
makemanback <makemanback@gmail.com>
2020-07-01 22:33:31 +0200

    исправил поведение карточек объявлений

--- js/card.js
+++ js/card.js
@@ -50,6 +50,8 @@
     var mapCardFeatures = mapCardFeaturesContainer.children;
     var mapCardPhotosContainer = mapCard.querySelector('.popup__photos');
 
+    // var popupClose = mapCard.querySelector('.popup__close');
+
     mapCardTitle.textContent = ad.offer.title;
     mapCardAdress.textContent = ad.offer.address;
     mapCardPrice.textContent = ad.offer.price + '₽/ночь';
@@ -85,31 +87,32 @@
     return mapCard;
   };
 
-renderMapCard = function (element) {
-  var fragmentSecond = document.createDocumentFragment();
-  fragmentSecond.appendChild(createMapCard(element));
-}
-
-  var renderMapCards = function () {
-    var fragmentSecond = document.createDocumentFragment();
-    for (var i = 0; i < window.pin.ads.length; i++) {
-      fragmentSecond.appendChild(createMapCard(window.pin.ads[i]));
-      window.htmlSelectors.map.insertBefore(fragmentSecond, window.htmlSelectors.mapFilters);
-
-      // window.pin.ads.forEach(function (item, index) {
-      //   item.addEventListener('click', function () {
-
-      //     //удалить прошлую карту
-      //     getPinCard(index); // здесь твой вызов - это не итоговый вариант, просто направление
-      //   });
-      // });
+  var removeCard = function () {
+    if (window.htmlSelectors.map.querySelector('.map__card')) {
+      window.htmlSelectors.map.querySelector('.map__card').remove();
     }
   };
+  var renderMapCard = function (element) {
+    var fragmentSecond = document.createDocumentFragment();
+    fragmentSecond.appendChild(createMapCard(element));
 
-  renderMapCards();
+    window.htmlSelectors.map.insertBefore(fragmentSecond, window.htmlSelectors.mapFilters);
+
+    var popupClose = document.querySelector('.popup__close');
+
+    popupClose.addEventListener('click', function () {
+      removeCard();
+    });
+    document.addEventListener('keydown', function (evt) {
+      evt.preventDefault();
+      if (evt.key === 'Escape') {
+        removeCard();
+      }
+    });
+  };
 
   window.card = {
-    renderMapCards: renderMapCards
-  }
-
+    renderMapCard: renderMapCard,
+    removeCard: removeCard
+  };
 })();
--- js/map.js
+++ js/map.js
@@ -4,22 +4,21 @@
   window.utils.makeElementsDisabled(window.htmlSelectors.mapFeatures);
   window.utils.makeElementsDisabled(window.htmlSelectors.mapFiltersSelects);
   window.utils.hideElements(window.renderedSelectors.mapPinsCollection);
-  window.utils.hideElements(window.renderedSelectors.mapCards);
 
   var onClickOpenCard = function (element, data) {
     element.addEventListener('click', function () {
-      data.classList.remove('hidden');
-      window.utils.makeElementsDisabled(window.renderedSelectors.mapPinsCollection);
-
+      window.card.removeCard();
+      window.card.renderMapCard(data);
     });
   };
+
 
   var onClickCloseCard = function (element, data) {
     element.addEventListener('click', function () {
 
       data.classList.add('hidden');
       window.utils.makeElementsAvailable(window.renderedSelectors.mapPinsCollection);
-      window.htmlSelectors.mapPinMain.setAttribute('disabled', true);
+
     });
   };
 
@@ -29,16 +28,17 @@
         evt.preventDefault();
         data.classList.add('hidden');
         window.utils.makeElementsAvailable(window.renderedSelectors.mapPinsCollection);
-        window.htmlSelectors.mapPinMain.setAttribute('disabled', true);
       }
     });
   };
 
   var openCard = function () {
-    for (var i = 0; i < window.renderedSelectors.mapCards.length; i++) {
-      onClickOpenCard(window.renderedSelectors.mapPinsCollection[i], window.renderedSelectors.mapCards[i]);
-    }
+    window.pin.ads.forEach(function (item, index) {
+      onClickOpenCard(window.renderedSelectors.mapPinsCollection[index], item);
+    });
   };
+
+  openCard();
 
   var closeCard = function () {
     for (var i = 0; i < window.renderedSelectors.mapCards.length; i++) {
@@ -49,13 +49,12 @@
   };
 
   var makePageActive = function () {
-    window.htmlSelectors.mapPinMain.setAttribute('disabled', true);
+
     window.htmlSelectors.map.classList.remove('map--faded');
     window.htmlSelectors.adForm.classList.remove('ad-form--disabled');
     window.utils.makeElementsAvailable(window.htmlSelectors.mapFeatures);
     window.utils.makeElementsAvailable(window.htmlSelectors.mapFilter);
 
-    openCard();
     closeCard();
     window.utils.showElements(window.renderedSelectors.mapPinsCollection);
   };
